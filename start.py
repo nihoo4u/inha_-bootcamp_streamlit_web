1943c1eaafa842c6e763a1cd236ba31ba28e9fda
pjy <nihoo4u@gmail.com>
2024-07-18 14:25:43 +0900

    delete map

--- start.py
+++ start.py
@@ -14,9 +14,9 @@ data=pd.DataFrame({'index':['A','B','C'],
                    'values':[10,20,30]})
 st.write('## chart2!!')
 st.bar_chart(data,x='index',y='values',use_container_width=True)
-m=folium.Map(location=[37.42637222,126.9898],zoom_start=16)
-folium.Marker([37.42637222,126.9898],
-              popup='junyeon',
-              tooltip='junyeon').add_to(m)
-st_data=st_folium(m,width=725)
+# m=folium.Map(location=[37.42637222,126.9898],zoom_start=16)
+# folium.Marker([37.42637222,126.9898],
+#               popup='junyeon',
+#               tooltip='junyeon').add_to(m)
+# st_data=st_folium(m,width=725)
 
