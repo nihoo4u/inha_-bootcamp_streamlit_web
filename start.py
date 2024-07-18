2a6b0cb295a04c0d33744b5039f578bfaf82cb8e
pjy <nihoo4u@gmail.com>
2024-07-18 11:00:57 +0900

    fisrt main commit

--- start.py
+++ start.py
@@ -9,5 +9,5 @@ sview= pd.Series(view)
 st.write('## gogo')
 data=pd.DataFrame({'index':['A','B','C'],
                    'values':[10,20,30]})
-st.write('## chart2!')
+st.write('## chart2!!')
 st.bar_chart(data,x='index',y='values',use_container_width=True)
