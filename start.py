fec5487b5844fe604dda9a13dda503b090cb2faa
pjy <nihoo4u@gmail.com>
2024-07-22 14:48:12 +0900

    gogo

--- start.py
+++ start.py
@@ -20,37 +20,52 @@
 # #               tooltip='junyeon').add_to(m)
 # # st_data=st_folium(m,width=725)
 
-import streamlit as st
-import pandas as pd
+# import streamlit as st
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+# # Load data
+# file_path = 'weather_real대전.csv'
+# df = pd.read_csv(file_path)
+
+# # Convert '시간' column to datetime format
+# df['시간'] = pd.to_datetime(df['시간'])
+
+# # Sidebar for user input
+# st.sidebar.title("Weather Data Visualization")
+# selected_columns = st.sidebar.multiselect('Select columns to plot', df.columns, default=['기온', '강수량', '풍속', '습도'])
+
+# # Main panel
+# st.title("Weather Data Analysis")
+
+# if selected_columns:
+#     st.write(f"## Plotting columns: {', '.join(selected_columns)}")
+
+#     for column in selected_columns:
+#         st.write(f"### {column}")
+#         plt.figure(figsize=(10, 4))
+#         plt.plot(df['시간'], df[column])
+#         plt.xlabel('Time')
+#         plt.ylabel(column)
+#         plt.title(f"{column} over Time")
+#         st.pyplot(plt)
+# else:
+#     st.write("Please select at least one column to plot from the sidebar.")
+
+# if __name__ == '__main__':
+#     st.run()
+
+
 import matplotlib.pyplot as plt
 
-# Load data
-file_path = 'weather_real대전.csv'
-df = pd.read_csv(file_path)
-
-# Convert '시간' column to datetime format
-df['시간'] = pd.to_datetime(df['시간'])
-
-# Sidebar for user input
-st.sidebar.title("Weather Data Visualization")
-selected_columns = st.sidebar.multiselect('Select columns to plot', df.columns, default=['기온', '강수량', '풍속', '습도'])
-
-# Main panel
-st.title("Weather Data Analysis")
-
-if selected_columns:
-    st.write(f"## Plotting columns: {', '.join(selected_columns)}")
-
-    for column in selected_columns:
-        st.write(f"### {column}")
-        plt.figure(figsize=(10, 4))
-        plt.plot(df['시간'], df[column])
-        plt.xlabel('Time')
-        plt.ylabel(column)
-        plt.title(f"{column} over Time")
-        st.pyplot(plt)
-else:
-    st.write("Please select at least one column to plot from the sidebar.")
-
-if __name__ == '__main__':
-    st.run()
+xdata = [1,2, 3,4]
+ydata = [10 ,5 ,20 ,35]
+#그래프
+plt.plot(xdata , ydata)
+plt.show()
+#막대형 그래프
+plt.bar(xdata , ydata)
+plt.show()
+#90도 회전한 막대 그래프
+plt.barh(xdata ,ydata)
+plt.show()
